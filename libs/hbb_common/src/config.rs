1bb1039d31279df3a0270a9aab6ae3e4f844c34c
qq9568488 <31581876+qq9568488@users.noreply.github.com>
2024-11-10 23:08:22 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rdk.lhjb.vip"];
-pub const PUBLIC_RS_PUB_KEY: &str = "4GYXnJ8lYk4dJlnqqzil8kcvwXZlPkwsIS3k3YQ3mKA=";
+pub const PUBLIC_RS_PUB_KEY: &str = "TnSDX07qR00Po4oGRSjvy62r07pCBDkPhPjzmmXt9jc=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
